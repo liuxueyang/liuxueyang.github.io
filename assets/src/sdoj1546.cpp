12b0445f39cba4a3a131f61dd25a8a079edff9df
liuxueyang <liuxueyang457@gmail.com>
2020-06-23 00:10:35 +0800

    finish sdoj1546

--- assets/src/sdoj1546.cpp
+++ assets/src/sdoj1546.cpp
@@ -19,30 +19,34 @@ int main() {
     int n;
     cin >> n;
     for (int i = 1; i <= n; i++) {
+      res[i] = false;
       for (int j = 1; j <= n; j++) {
         cin >> a[i][j];
       }
     }
+
     for (int _i = 0; _i < n; _i++) {
       int b1[N];
       for (int i = 1; i <= n; i++) b[i] = b1[i] = i;
       for (int i = _i + 1, j = 1; i <= n; i++, j++) b[j] = b[i];
-      for (int i = 1, j = n - _i + 1; j <= n; i++, j++) b[j] = b[i];
+      for (int i = 1, j = n - _i + 1; j <= n; i++, j++) b[j] = b1[i];
       b[n+1] = b[1];
       for (int i = 1; i <= n + 1; i++)
         for (int j = 1; j <= n + 1; j++)
           d[i][j] = false;
 
-      for (int len = 1; len < n; len++) {
-        for (int i = 1; i < n + 1; i++) {
+      for (int len = 1; len <= n; len++) {
+        for (int i = n; i >= 1; i--) {
           int j = i + len;
+          if (j > n + 1) continue;
           if (len == 1) {
-            d[i][j] = true;
+            d[i][j] = d[j][i] = true;
             continue;
           }
           for (int m = i + 1; m < j; m++) {
-            if (a[b[j]][b[m]] || a[b[i]][b[m]]) {
-              d[i][j] = true;
+            if ( (d[j][m] && d[i][m]) &&
+                 (a[b[j]][b[m]] || a[b[i]][b[m]])) {
+              d[i][j] = d[j][i] = true;
               break;
             }
           }
@@ -56,7 +60,7 @@ int main() {
       cout << (res[i] ? 1 : 0) << endl;
     }
 
-    if (tt != T) cout << endl;
+    if (tt != T - 1) cout << endl;
   }
 
   return 0;
