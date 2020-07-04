f3ebdd0cf42bbe50eff601398b0323bcb7a41da7
liuxueyang <liuxueyang457@gmail.com>
2020-07-04 20:01:41 +0800

    simple problem solutions on ybt

---
+++ assets/src/ybt/1106.cpp
@@ -0,0 +1,110 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+#define oo ((1LL<<31) - 10);
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1106.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  double s[4];
+  int n, a[120];
+  cin >> n;
+  for (int i = 0; i < 4; i++) s[i] = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    if (a[i] <= 18) s[0]++;
+    else if (a[i] <= 35) s[1]++;
+    else if (a[i] <= 60) s[2]++;
+    else s[3]++;
+  }
+  cout.precision(2);
+  cout.setf(ios::fixed, ios::floatfield);
+  for (int i = 0; i < 4; i++)
+    {
+      cout << s[i] / n * 100 << "%\n";
+    }
+
+  return 0;
+}
