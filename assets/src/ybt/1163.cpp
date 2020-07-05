04d68617623de72c6700c1274f8bdf238e5ad815
liuxueyang <liuxueyang457@gmail.com>
2020-07-05 23:47:16 +0800

    finish recursive section on ybt

---
+++ assets/src/ybt/1163.cpp
@@ -0,0 +1,111 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+#define oo ((1LL<<31) - 10);
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+int pro(int m, int n)
+{
+  if(!m)
+    {
+      return n+1;
+    }
+  if(m>0&&!n)
+    {
+      return pro(m-1,1);
+    }
+  else if(m>0&&n>0)
+    {
+      return pro(m-1,pro(m,n-1));
+    }
+  return 0;
+}
+
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1163.in", "r", stdin);
+#endif
+
+  int m,n;
+  cin>>m>>n;
+  cout<<pro(m,n)<<endl;
+
+  return 0;
+}
---
+++ assets/src/ybt/1167.cpp
@@ -0,0 +1,108 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+#define oo ((1LL<<31) - 10);
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+double pro(double x,int n)
+{
+  if(n==1)
+    {
+      return x/(1+x);
+    }
+  return x/(n+pro(x,n-1));
+}
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1167.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  double x;
+  int n;
+  cin>>x>>n;
+  cout.precision(2);
+  cout.setf(ios::fixed,ios::floatfield);
+  cout<<pro(x,n)<<endl;
+
+  return 0;
+}
