6c5db58e12a303ec010868a1d205d39c3a8709e6
liuxueyang <liuxueyang457@gmail.com>
2020-07-17 00:12:09 +0800

    finish 1312

---
+++ assets/src/ybt/1312.cpp
@@ -0,0 +1,104 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+#define oo ((1LL<<31) - 10);
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+LL a[100],b[100];
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1312.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  LL x,y,z;
+  cin>>x>>y>>z;
+  for(int i=1;i<=x;i++)
+    a[i]=1,b[i]=0;
+  for(int i=x+1;i<=z+1;i++){
+    b[i]=y*a[i-x];
+    a[i]=a[i-1]+b[i-2];
+  }
+  cout<<a[z+1]<<endl;
+
+  return 0;
+}
