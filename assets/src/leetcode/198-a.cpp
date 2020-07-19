ad31f8edd346032df14725d461a1d0f9d15e989f
liuxueyang <liuxueyang457@gmail.com>
2020-07-19 12:04:37 +0800

    leetcode contest 198

---
+++ assets/src/leetcode/198-a.cpp
@@ -0,0 +1,129 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <tuple>
+#include <string>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <type_traits>
+#include <iterator>
+#include <limits>
+#include <stdexcept>
+#include <random>
+#include <chrono>
+
+// ==================================================
+
+using namespace std;
+
+using PII = pair<int, int>;
+using LL = long long;
+using VI = vector<int>;
+static const LL oo = (1LL<<31);
+
+// ==================================================
+
+static auto _2333_ = []() {
+                       ios::sync_with_stdio(false);
+                       cin.tie(nullptr); return 0; }();
+
+// ==================================================
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINT_CONTAINER(const T& c) { for (auto &x : c) PRINTC(x); NL; }
+
+template<typename T>
+void PRINTV(const vector<T>& c) { PRINT_CONTAINER<vector<T>>(c); }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+class Solution {
+public:
+  int helper(int emp, int exc) {
+    if(emp < exc) return 0;
+    int t = emp / exc;
+    return t + helper(t + emp % exc, exc);
+  }
+    int numWaterBottles(int numBottles, int numExchange) {
+      if(numBottles < numExchange) return numBottles;
+      return numBottles + helper(numBottles, numExchange);
+    }
+};
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("198-a.in", "r", stdin);
+#endif
+
+  Solution a;
+  cout<<a.numWaterBottles(9, 3)<<endl;
+  cout<<a.numWaterBottles(15, 4)<<endl;
+  cout<<a.numWaterBottles(5, 5)<<endl;
+  cout<<a.numWaterBottles(2, 3)<<endl;
+
+  return 0;
+}
