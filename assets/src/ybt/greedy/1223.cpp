b90f27876d149f81d9b784ff21b2354b5ba8c98d
liuxueyang <liuxueyang457@gmail.com>
2020-07-28 23:37:42 +0800

    start greedy chapter

---
+++ assets/src/ybt/greedy/1223.cpp
@@ -0,0 +1,138 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+
+__attribute__((unused)) const static int dir[8][2] = {
+  {0, 1}, {1, 0},
+  {0, -1},{-1, 0},
+  {1, 1}, {1, -1},
+  {-1,1}, {-1,-1},
+};
+
+// ==================================================
+#define oo (1LL<<31);
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+int c1(int a) {
+  int res=0;
+  while (a) {
+    if (a & 1) ++res;
+    a >>= 1;
+  }
+  return res;
+}
+
+int c2(int a) {
+  int res=0, pos=0;
+  while (a) {
+    if ((a&1) && ((a-1)%4==0)) {
+      ++a;
+      a<<=pos;
+      a+=((1<<res)-1);
+      return a;
+    }
+    if (a&1) ++res;
+    ++pos;
+    a>>=1;
+  }
+  return a;
+}
+
+int solve(int n) {
+  int c = c1(n);
+  if (c == 1) return n<<1;
+  return c2(n);
+}
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1223.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n; while (cin >> n) {
+    if (!n) break;
+    cout << solve(n) << endl;
+  }
+
+  return 0;
+}
