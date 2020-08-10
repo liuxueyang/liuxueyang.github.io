2f1bdf0adb1d5c2c3a2b34907d95afb5554405ad
liuxueyang <liuxueyang457@gmail.com>
2020-08-11 00:26:18 +0800

    1246 WIP

---
+++ assets/src/ybt/divide_conquer/1242.cpp
@@ -0,0 +1,169 @@
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+
+__attribute__((unused)) const static int dir[8][2] = {
+                                                      {0, 1}, {1, 0},
+                                                      {0, -1},{-1, 0},
+                                                      {1, 1}, {1, -1},
+                                                      {-1,1}, {-1,-1},
+};
+
+// ==================================================
+#define oo (1LL<<31)
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+const int N = 10000+100;
+int a[N], K, n;
+
+int bcnt(int len) {
+  if (len == 0) {
+    return 0;
+  }
+  int l = 1, r = n, mid, pos, res = 0;
+  while (l + 1 < r) {
+    mid = (l+r)/2;
+    if (a[mid] >= len) {
+      r = mid;
+    } else {
+      l = mid+1;
+    }
+  }
+  pos = (a[l] >= mid ? l : r);
+  for (int i = pos; i <= n; ++i) {
+    res += a[i] / len;
+  }
+  return res;
+}
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1242.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  /*
+   * sample input:
+   1 3
+   0.02
+  */
+
+  cin >> n >> K;
+  double t;
+  int low = 0, high = 0, mid, cnt;
+  for (int i = 1; i <= n; ++i) {
+    cin >> t;
+    a[i] = int(floor(t*100.0));
+    high = max_(high, a[i]);
+  }
+
+  sort(a+1, a+1+n);
+  cout.precision(2);
+  cout.setf(ios::floatfield, ios::fixed);
+
+  while (low + 1 < high) {
+    mid = (low+high)/2;
+    if (mid == 0) {
+      break;
+    }
+    cnt = bcnt(mid);
+    if (cnt >= K) {
+      low = mid;
+    } else {
+      high = mid-1;
+    }
+  }
+  cnt = bcnt(high);
+
+  if (cnt >= K) {
+    cout << high*1.0/100 << "\n";
+  } else {
+    if (bcnt(low) >= K) {
+      cout << low*1.0/100 << "\n";
+    } else {
+      cout << "0.00" << "\n";
+    }
+  }
+
+
+  return 0;
+}
