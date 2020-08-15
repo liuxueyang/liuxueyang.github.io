4478c7449d78386372525c0b71f63db558e7f095
liuxueyang <liuxueyang457@gmail.com>
2020-08-15 17:24:12 +0800

    finish 3 simple dp problems

---
+++ assets/src/ybt/dp/1261.cpp
@@ -0,0 +1,191 @@
+// 1261.cpp --- 1261：【例9.5】城市交通路网
+// 
+// Filename: 1261.cpp
+// Description: 
+// Author: read eval print loop
+// Maintainer: 
+// Created: Sat Aug 15 16:45:53 2020 (+0800)
+// Version: 
+// Last-Updated: Sat Aug 15 17:19:56 2020 (+0800)
+//           By: read eval print loop
+//     Update #: 1
+// URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 1A
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+// Floor, Boston, MA 02110-1301, USA.
+// 
+// 
+
+// Code:
+
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+
+__attribute__((unused)) const static int dir[8][2] = {
+  {0, 1}, {1, 0},
+  {0, -1},{-1, 0},
+  {1, 1}, {1, -1},
+  {-1,1}, {-1,-1},
+};
+
+// ==================================================
+#define oo (1LL<<31)
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+const int N = 123;
+LL a[N][N], path[N], d[N], n;
+
+void print_path(int pos) {
+  if (pos == 1) {
+    cout << pos << "";
+    return;
+  }
+  print_path(path[pos]);
+  if (pos != 1) {
+    cout << " " << pos; 
+  }
+}
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1261.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n;
+  for (int i = 1; i < n + 1; ++i) {
+    for (int j = 1; j < n + 1; ++j) {
+      cin >> a[i][j];
+    }
+  }
+
+  d[1] = 0;
+  path[n] = n;
+  for (int i = 2; i <= n; ++i) {
+    d[i] = oo;
+    for (int j = 1; j <= n; ++j) {
+      if (i != j && a[j][i] && d[j] != oo) {
+        if (d[j] + a[j][i] < d[i]) {
+          d[i] = d[j] + a[j][i];
+          path[i] = j;
+        }
+      }
+    }
+  }
+  cout << "minlong=" << d[n] << "\n";
+  print_path(n);
+  NL;
+
+  return 0;
+}
+
+
+// 
+// 1261.cpp ends here
