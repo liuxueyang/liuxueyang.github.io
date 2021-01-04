369f02264f6eeef2a9328a9f84ae2b91e75acedb
liuxueyang <liuxueyang457@gmail.com>
2021-01-04 21:25:47 +0800

    resolve 1284, 1285, 1286

---
+++ assets/src/ybt/dp/1284_1.cpp
@@ -0,0 +1,186 @@
+// 1284_1.cpp --- 
+// 
+// Filename: 1284_1.cpp
+// Description: 
+// Author: 刘雪杨
+// Maintainer: 
+// Created: Mon Jan  4 20:40:47 2021 (+0800)
+// Version: 
+// Last-Updated: 
+//           By: 
+//     Update #: 0
+// URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+// Floor, Boston, MA 02110-1301, USA.
+// 
+// 
+
+// Code:
+
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+
+__attribute__((unused)) const static int dir[8][2] = {
+  {0, 1}, {1, 0},
+  {0, -1},{-1, 0},
+  {1, 1}, {1, -1},
+  {-1,1}, {-1,-1},
+};
+
+// ==================================================
+#define oo (1LL<<31)
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+const int N = 120;
+int a[N][N], d[N][N], res, R, C;
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1284.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  cin >> T;
+  while (T--) {
+    cin >> R >> C;
+    memset(a, 0, sizeof a);
+    memset(d, 0, sizeof d);
+    int tmp;
+    for (int i = 0; i < R; ++i) {
+      for (int j = 0; j < C; ++j) {
+        cin >> a[i][j];
+      }
+    }
+    d[0][0] = a[0][0];
+    for (int i = 0; i < R; ++i) {
+      for (int j = 0; j < C; ++j) {
+        if (i + j == 0) continue;
+        tmp = d[i][j];
+        if (i > 0) {
+          tmp = d[i-1][j] + a[i][j];
+          if (tmp > d[i][j]) d[i][j] = tmp;
+        }
+        if (j > 0) {
+          tmp = d[i][j-1] + a[i][j];
+          if (tmp > d[i][j]) d[i][j] = tmp;
+        }
+      }
+    }
+    cout << d[R-1][C-1] << endl;
+  }
+
+  return 0;
+}
+
+
+// 
+// 1284_1.cpp ends here
---
+++ assets/src/ybt/dp/1285_1.cpp
@@ -0,0 +1,176 @@
+// 1285_1.cpp --- 
+// 
+// Filename: 1285_1.cpp
+// Description: 
+// Author: 刘雪杨
+// Maintainer: 
+// Created: Mon Jan  4 21:09:21 2021 (+0800)
+// Version: 
+// Last-Updated: 
+//           By: 
+//     Update #: 0
+// URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+// Floor, Boston, MA 02110-1301, USA.
+// 
+// 
+
+// Code:
+
+// ==================================================
+
+// C library
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// Containers
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+
+// Input/Output
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
+#include <iomanip>
+
+// Other
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <limits>
+
+// ==================================================
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// ==================================================
+
+__attribute__((unused)) const static int dir[8][2] = {
+  {0, 1}, {1, 0},
+  {0, -1},{-1, 0},
+  {1, 1}, {1, -1},
+  {-1,1}, {-1,-1},
+};
+
+// ==================================================
+#define oo (1LL<<31)
+#define max_(x, y) ((x) > (y) ? (x) : (y))
+#define min_(x, y) ((x) > (y) ? (y) : (x))
+
+#define PR(x) cout << #x " = " << (x) << "\t"
+#define NL cout << "\n"
+
+#define PRINT1(x) PR(x), NL
+#define PRINT2(x1, x2) PR(x1), PRINT1(x2)
+#define PRINT3(x1, x2, x3) PR(x1), PRINT2(x2, x3)
+#define PRINT4(x1, x2, x3, x4) PR(x1), PRINT3(x2, x3, x4)
+
+// ==================================================
+
+template<typename T>
+void PRINTC(const T& a) { cout << a << " "; }
+
+template<typename T>
+void PRINTA(const T ar[], int n) {
+  for (int i = 0; i < n; ++i) PRINTC(ar[i]); NL;
+}
+
+template<typename T1, typename T2>
+void PRINTP(const pair<T1, T2>& p) { PRINTC(p.first); PRINTLN(p.second); }
+
+template<typename T>
+void PRINTLN(const T& a) { cout << a << "\n"; }
+
+template< typename T1, typename T2 >
+void PRINTAV( T1 & vec, T2 x) {
+  ostream_iterator< T2 > O( cout, " " );
+  copy( begin( vec ), end( vec ), O ); NL;
+}
+
+// ==================================================
+
+
+const int N = 1233;
+int a[N], d[N], tmp, res, n;
+
+int main( void ) {
+
+#ifdef DEBUG
+  freopen("1285.in", "r", stdin);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n;
+  memset(d, 0, sizeof d);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    d[i] = a[i];
+  }
+  res = 0;
+  for (int i = 1; i < n; ++i) {
+    for (int j = 0; j < i; ++j) {
+      if (a[j] < a[i]) {
+        tmp = d[j] + a[i];
+        if (tmp > d[i]) {
+          d[i] = tmp;
+          if (res < d[i]) res = d[i];
+        }
+      }
+    }
+  }
+  cout << res << endl;
+
+  return 0;
+}
+
+
+// 
+// 1285_1.cpp ends here
