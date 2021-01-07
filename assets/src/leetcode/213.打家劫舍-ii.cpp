8432c88eb55c2a13fd8d91c7f0890bb4c8ed1bf7
liuxueyang <liuxueyang457@gmail.com>
2021-01-07 10:51:25 +0800

    finish leetcode 213

--- assets/src/leetcode/213.打家劫舍-ii.cpp
+++ assets/src/leetcode/213.打家劫舍-ii.cpp
@@ -16,11 +16,28 @@ class Solution {
 public:
     int rob(vector<int>& a) {
         int len = int(a.size()), res = 0;
-        if (len == 0 || len == 2) return res;
+        if (len == 0) return res;
         if (len == 1) return a[0];
         int d[110];
+
         memset(d, 0, sizeof(d));
-        
+        // use a[0]
+        d[0] = d[1] = a[0];
+        res = max(res, d[0]);
+        for (int i = 2; i < len - 1; i++) {
+            d[i] = max(d[i-1], d[i-2] + a[i]);
+            res = max(res, d[i]);
+        }
+
+        // don't use a[0]
+        memset(d, 0, sizeof d);
+        d[1] = a[1];
+        res = max(res, d[1]);
+        for (int i = 2; i < len; i++) {
+            d[i] = max(d[i-1], d[i-2] + a[i]);
+            res = max(res, d[i]);
+        }
+        return res;
     }
 };
 // @lc code=end
