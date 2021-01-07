8f83b82b77d3082fc4526c332c7adc1ec7b39b48
liuxueyang <liuxueyang457@gmail.com>
2021-01-07 22:37:39 +0800

    finish leetcode 1053

--- assets/src/leetcode/1053.交换一次的先前排列.cpp
+++ assets/src/leetcode/1053.交换一次的先前排列.cpp
@@ -4,11 +4,35 @@
  * [1053] 交换一次的先前排列
  */
 
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 // @lc code=start
 class Solution {
 public:
     vector<int> prevPermOpt1(vector<int>& arr) {
-
+        int len = int(arr.size());
+        if (len <= 1) return arr;
+        int pos = -1;
+        for (int i = len - 2; i >= 0; --i) {
+            if (arr[i] > arr[i + 1]) {
+                pos = i;
+                break;
+            }
+        }
+        if (pos < 0) { return arr; }
+        int cur = pos + 1;
+        for (int i = pos + 2; i < len; ++i) {
+            if (arr[i] < arr[pos]) {
+                if (arr[i] > arr[cur]) {
+                    cur = i;
+                }
+            }
+        }
+        swap(arr[pos], arr[cur]);
+        return arr;
     }
 };
 // @lc code=end
