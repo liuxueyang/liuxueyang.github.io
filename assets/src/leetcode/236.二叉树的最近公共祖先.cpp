5329b6240ebcdfc397e00fa4b413659c9b69320c
liuxueyang <liuxueyang457@gmail.com>
2021-01-08 22:12:34 +0800

    finish leetcode 236

--- assets/src/leetcode/236.二叉树的最近公共祖先.cpp
+++ assets/src/leetcode/236.二叉树的最近公共祖先.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -28,13 +30,42 @@ using namespace std;
  */
 class Solution {
 public:
-    void find_node(vector<int>& v, int val, TreeNode * r) {
+    set<int> ns;
+    void find_node(vector<TreeNode*>& v, int val, TreeNode * r) {
+        if (r == nullptr || v.size() > 0) return;
+        if (r->val == val) {
+            v.push_back(r);
+            return;
+        }
+        ns.insert(r->val);
 
+        find_node(v, val, r->left);
+        find_node(v, val, r->right);
+        if (v.size() > 0) {
+            v.push_back(r);
+        }
+
+        ns.erase(r->val);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         int r1 = p->val, r2 = q->val;
-        vector<int> v1, v2;
+        vector<TreeNode*> v1, v2;
+        find_node(v1, r1, root);
+        ns.clear();
+        find_node(v2, r2, root);
+        reverse(v1.begin(), v1.end());
+        reverse(v2.begin(), v2.end());
 
+        int len = min(v1.size(), v2.size());
+        TreeNode* res = nullptr;
+        for (int i = 0; i < len; ++i) {
+            if (v1[i]->val == v2[i]->val) {
+                res = v1[i];
+            } else {
+                break;
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
