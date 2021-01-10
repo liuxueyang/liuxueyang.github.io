51490a998c4e72726f41790b2a6beceb8ca9bbee
liuxueyang <liuxueyang457@gmail.com>
2021-01-10 21:38:08 +0800

    add leetcode 894

---
+++ assets/src/leetcode/894.所有可能的满二叉树.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=894 lang=cpp
+ *
+ * [894] 所有可能的满二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int N) {
+
+    }
+};
+// @lc code=end
+
