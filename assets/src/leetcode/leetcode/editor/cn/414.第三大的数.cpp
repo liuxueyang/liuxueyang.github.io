e9f1578ced3c939ef78a699ee3e0fe23f286fe50
liuxueyang <liuxueyang457@gmail.com>
2021-01-12 11:17:32 +0800

    finish leetcode 539

---
+++ assets/src/leetcode/leetcode/editor/cn/414.第三大的数.cpp
@@ -0,0 +1,52 @@
+//给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[3, 2, 1]
+//输出：1
+//解释：第三大的数是 1 。 
+//
+// 示例 2： 
+//
+// 
+//输入：[1, 2]
+//输出：2
+//解释：第三大的数不存在, 所以返回最大的数 2 。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：[2, 2, 3, 1]
+//输出：1
+//解释：注意，要求返回第三大的数，是指第三大且唯一出现的数。
+//存在两个值为2的数，它们都排第二。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 104 
+// 231 <= nums[i] <= 231 - 1 
+// 
+//
+// 
+//
+// 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？ 
+// Related Topics 数组 
+// 👍 197 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ assets/src/leetcode/leetcode/editor/cn/919.完全二叉树插入器.cpp
@@ -0,0 +1,74 @@
+//完全二叉树是每一层（除最后一层外）都是完全填充（即，节点数达到最大）的，并且所有的节点都尽可能地集中在左侧。 
+//
+// 设计一个用完全二叉树初始化的数据结构 CBTInserter，它支持以下几种操作： 
+//
+// 
+// CBTInserter(TreeNode root) 使用头节点为 root 的给定树初始化该数据结构； 
+// CBTInserter.insert(int v) 向树中插入一个新节点，节点类型为 TreeNode，值为 v 。使树保持完全二叉树的状态，并返回插入的
+//新节点的父节点的值； 
+// CBTInserter.get_root() 将返回树的头节点。 
+// 
+//
+// 
+//
+// 
+// 
+//
+// 示例 1： 
+//
+// 输入：inputs = ["CBTInserter","insert","get_root"], inputs = [[[1]],[2],[]]
+//输出：[null,1,[1,2]]
+// 
+//
+// 示例 2： 
+//
+// 输入：inputs = ["CBTInserter","insert","insert","get_root"], inputs = [[[1,2,3,4
+//,5,6]],[7],[8],[]]
+//输出：[null,3,4,[1,2,3,4,5,6,7,8]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 最初给定的树是完全二叉树，且包含 1 到 1000 个节点。 
+// 每个测试用例最多调用 CBTInserter.insert 操作 10000 次。 
+// 给定节点或插入节点的每个值都在 0 到 5000 之间。 
+// 
+// Related Topics 树 
+// 👍 36 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class CBTInserter {
+public:
+    CBTInserter(TreeNode* root) {
+
+    }
+    
+    int insert(int v) {
+
+    }
+    
+    TreeNode* get_root() {
+
+    }
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter* obj = new CBTInserter(root);
+ * int param_1 = obj->insert(v);
+ * TreeNode* param_2 = obj->get_root();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
