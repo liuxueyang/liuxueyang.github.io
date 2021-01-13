8538d84ccd2d5093b9a6aa8d13f133d6d91145f7
liuxueyang <liuxueyang457@gmail.com>
2021-01-13 13:26:40 +0800

    finish leetcode 673

--- assets/src/leetcode/leetcode/editor/cn/673.最长递增子序列的个数.cpp
+++ assets/src/leetcode/leetcode/editor/cn/673.最长递增子序列的个数.cpp
@@ -20,12 +20,55 @@
 // Related Topics 动态规划 
 // 👍 256 👎 0
 
+// 修改一下最长子序列长度的做法即可
+
 
 //leetcode submit region begin(Prohibit modification and deletion)
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+using VI = vector<int>;
+using VS = vector<string>;
+
 class Solution {
 public:
-    int findNumberOfLIS(vector<int>& nums) {
+    int d[2020], c[2020];
 
+    int findNumberOfLIS(vector<int> &a) {
+        int len = a.size();
+        memset(d, 0, sizeof(d));
+        memset(c, 0, sizeof(c));
+        int maxlen = 1, res = 0;
+        for (int i = 0; i < len; ++i) d[i] = c[i] = 1;
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (a[i] > a[j]) {
+                    if (d[j] + 1 > d[i]) {
+                        d[i] = d[j] + 1;
+                        maxlen = max(maxlen, d[i]);
+                        c[i] = c[j];
+                    } else if (d[j] + 1 == d[i]) {
+                        c[i] += c[j];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < len; ++i) {
+            if (maxlen == d[i]) {
+                res += c[i];
+            }
+        }
+        return res;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
