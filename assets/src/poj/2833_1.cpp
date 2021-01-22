78f9b8a05350e6cf83ce8a92b1215e0eef8e6b5a
liuxueyang <liuxueyang457@gmail.com>
2021-01-22 15:36:40 +0800

    optimize 2833

--- assets/src/poj/2833_1.cpp
+++ assets/src/poj/2833_1.cpp
@@ -8,66 +8,33 @@ using namespace std;
 int sz1, sz2, h1[30], h2[30], n1, n2;
 
 // min heap
-void Down1(int x) {
+void Down1(int x, int h1[], int& sz1) {
   int t = x, l = x * 2, r = l + 1;
   if (l <= sz1 && h1[l] < h1[t]) t = l;
   if (r <= sz1 && h1[r] < h1[t]) t = r;
   if (t == x) return;
   swap(h1[t], h1[x]);
-  Down1(t);
+  Down1(t, h1, sz1);
 }
 
-int Pop1() {
+int Pop1(int h1[], int& sz1) {
   int res = h1[1];
   h1[1] = h1[sz1--];
-  Down1(1);
+  Down1(1, h1, sz1);
   return res;
 }
 
-void Up1(int x) {
+void Up1(int x, int h1[]) {
   while (x / 2 && h1[x / 2] > h1[x]) {
     swap(h1[x / 2], h1[x]);
     x /= 2;
   }
 }
 
-void Push1(int x) {
+void Push1(int x, int h1[], int& sz1, int n1) {
   h1[++sz1] = x;
-  Up1(sz1);
-  if (sz1 > n1) Pop1();
-}
-
-// max heap
-void Down2(int x) {
-  int t = x, l = x * 2, r = l + 1;
-  if (l <= sz2 && h2[l] > h2[t])
-    t = l;
-  if (r <= sz2 && h2[r] > h2[t])
-    t = r;
-  if (t == x) return;
-  swap(h2[t], h2[x]);
-  Down2(t);
-}
-
-int Pop2() {
-  int res = h2[1];
-  h2[1] = h2[sz2--];
-  Down2(1);
-  return res;
-}
-
-void Up2(int x) {
-  while (x / 2 && h2[x / 2] < h2[x]) {
-    swap(h2[x / 2], h2[x]);
-    x /= 2;
-  }
-}
-
-void Push2(int x) {
-  h2[++sz2] = x;
-  Up2(sz2);
-  if (sz2 > n2)
-    Pop2();
+  Up1(sz1, h1);
+  if (sz1 > n1) Pop1(h1, sz1);
 }
 
 int main() {
@@ -90,15 +57,16 @@ int main() {
       sum += tmp;
 
       if (sz1 < n1 || tmp >= h1[1]) {
-        Push1(tmp);
+        Push1(tmp, h1, sz1, n1);
       }
-      if (sz2 < n2 || tmp <= h2[1]) {
-        Push2(tmp);
+      if (sz2 < n2 || -tmp >= h2[1]) {
+        Push1(-tmp, h2, sz2, n2);
       }
     }
 
-    while (sz1) sum -= Pop1();
-    while (sz2) sum -= Pop2();
+    while (sz1) sum -= Pop1(h1, sz1);
+    while (sz2) sum += Pop1(h2, sz2);
+
     double res = 0;
     if (n != n1 + n2) {
       res = sum / (n - n1 - n2);
