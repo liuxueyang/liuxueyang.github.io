1669bcbb8ab2f7d0bb17a7d641935dd11d6d2e67
liuxueyang <liuxueyang457@gmail.com>
2021-02-05 22:37:14 +0800

    finish leetcode 1624

--- assets/src/leetcode/1131.绝对值表达式的最大值.cpp
+++ assets/src/leetcode/1131.绝对值表达式的最大值.cpp
@@ -5,6 +5,27 @@
  */
 
 // @lc code=start
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iomanip>
+
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+using VI = vector<int>;
+using VS = vector<string>;
+
 class Solution {
 public:
     int maxAbsValExpr(vector<int>& arr1, vector<int>& arr2) {
