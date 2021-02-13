3c82540d77169cb39c84dd48b5053e722fd5be70
liuxueyang <liuxueyang457@gmail.com>
2021-02-13 21:00:33 +0800

    finish luogu 3853

---
+++ assets/src/luogu/3853_2.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 100010;
+int a[N], L, n, k;
+
+int bsearch(int l, int r) {
+  int mid;
+  while (l < r) {
+    mid = (l + r) >> 1;
+
+    int cnt = 0;
+    bool flag = true;
+    for (int i = 1; i < n; ++i) {
+      int gap = a[i] - a[i - 1];
+      cnt += gap / mid;
+      if (gap % mid == 0) --cnt;
+      if (cnt > k) { flag = false; break; }
+    }
+
+    if (flag) r = mid;
+    else l = mid + 1;
+  }
+  return r;
+}
+
+int main() {
+#ifdef _DEBUG
+  freopen("3853.in", "r", stdin);
+#endif
+  scanf("%d%d%d", &L, &n, &k);
+  for (int i = 0; i < n; ++i) scanf("%d", a + i);
+  sort(a, a + n);
+  printf("%d\n", bsearch(1, L));
+
+  return 0;
+}
