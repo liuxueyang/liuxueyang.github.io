0924a8caff2b0d32a2f48e7e6edc207e1de35980
liuxueyang <liuxueyang457@gmail.com>
2021-02-19 00:11:20 +0800

    finish leetcode 82

--- assets/src/leetcode/82.删除排序链表中的重复元素II_1.cpp
+++ assets/src/leetcode/82.删除排序链表中的重复元素II_1.cpp
@@ -8,19 +8,6 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-#include <cstdio>
-
-using namespace std;
-
 class Solution {
 public:
   ListNode* truncate(ListNode* h) {
@@ -28,44 +15,29 @@ public:
 
     ListNode* res = h;
     bool flag = false;
+
     while (res->next != nullptr && res->next->val == res->val) {
-      res = res->next;
       flag = true;
+      res = res->next;
     }
+
     if (flag) res = res->next;
-    if (h == res) return res;
+
+    if (res == h) return res;
     else return truncate(res);
   }
 
   ListNode* deleteDuplicates(ListNode* head) {
     ListNode* res = new ListNode(0, head);
-    ListNode* cur = res->next, *prev = res;
+    ListNode* prev = res, *cur = head;
 
     while (cur != nullptr) {
       cur = truncate(cur);
       prev->next = cur;
       prev = cur;
-      if (cur != nullptr)
-        cur = cur->next;
+      if (cur != nullptr) cur = cur->next;
     }
 
     return res->next;
   }
 };
-
-int main() {
-  Solution a;
-  int v[7] = {1, 2, 3, 3, 4, 4, 5};
-  ListNode* h = new ListNode();
-  ListNode* cur = h;
-  for (int i = 0; i < 7; ++i) {
-    ListNode* t = new ListNode(v[i]);
-    cur->next = t;
-    cur = t;
-    printf("insert: %d\n", cur->val);
-  }
-
-  a.deleteDuplicates(h->next);
-
-  return 0;
-}
