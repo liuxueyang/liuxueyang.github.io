d16a354d51ac602ba044282d26d312eb205fc231
liuxueyang <liuxueyang457@gmail.com>
2021-02-25 09:09:44 +0800

    wip luogu 2895

---
+++ assets/src/luogu/2895.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+int main() {
+  #ifdef _DEBUG
+  freopen("2895.in", "r", stdin);
+  #endif
+
+
+
+  return 0;
+}
