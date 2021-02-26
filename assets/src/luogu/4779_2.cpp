f52321b1faf0e0d74a115b3b6e03813069c10d79
liuxueyang <liuxueyang457@gmail.com>
2021-02-26 23:27:28 +0800

    finish luogu 4779 again

---
+++ assets/src/luogu/4779_2.cpp
@@ -0,0 +1,62 @@
+#include <queue>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+using PII = pair<int, int>;
+using ll = long long;
+
+const int N = 100010, M = 200010, INF = 0x3f3f3f3f;
+int n, m, s, st[N], h[N], dis[N];
+int e[M], ne[M], w[M], idx;
+
+void Init() {
+  idx = 0;
+  memset(dis, INF, sizeof dis);
+  memset(st, 0, sizeof st);
+  memset(h, -1, sizeof h);
+}
+
+void Add(int a, int b, int c) {
+  e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+
+void dijkstra() {
+  priority_queue<PII, vector<PII>, greater<PII>> q;
+  dis[s] = 0;
+  q.push({0, s});
+  while (!q.empty()) {
+    PII t = q.top(); q.pop();
+    int ver = t.second, base = t.first;
+    if (st[ver]) continue;
+    st[ver] = 1;
+    for (int i = h[ver]; i != -1; i = ne[i]) {
+      int j = e[i];
+      if (dis[j] > base + w[i]) {
+        dis[j] = base + w[i];
+        q.push({dis[j], j});
+      }
+    }
+  }
+}
+
+int main() {
+  #ifdef _DEBUG
+  freopen("4779.in", "r", stdin);
+  #endif
+
+  scanf("%d%d%d", &n, &m, &s);
+  Init();
+  while (m--) {
+    int a, b, c; scanf("%d%d%d", &a, &b, &c);
+    Add(a, b, c);
+  }
+  dijkstra();
+  for (int i = 1; i <= n; ++i) {
+    printf("%lld ", dis[i] == INF ? (1LL<<31) - 1 : dis[i]);
+  }
+  puts("");
+
+  return 0;
+}
---
+++ assets/src/luogu/4779_3.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+using PII = pair<int, int>;
+
+const int N = 100010, M = 200010, INF = 0x3f3f3f3f;
+int dis[N], st[N], n, m, s, h[N];
+int e[M], ne[M], w[M], idx;
+
+void Init() {
+  idx = 0; memset(dis, INF, sizeof dis);
+  memset(st, 0, sizeof st);
+  memset(h, -1, sizeof h);
+}
+
+void dijkstra() {
+  dis[s] = 0; priority_queue<PII, vector<PII>, greater<PII>> q; q.push({0, s});
+  while (!q.empty()) {
+    PII t = q.top(); q.pop();
+    int ver = t.second, base = t.first;
+    if (st[ver]) continue;
+    st[ver] = 1;
+    for (int i = h[ver], j; i != -1; i = ne[i]) {
+      j = e[i];
+      if (base + w[i] < dis[j]) { dis[j] = base + w[i]; q.push({dis[j], j}); }
+    }
+  }
+}
+
+void Add(int a, int b, int c) {
+  e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+
+int main() {
+  #ifdef _DEBUG
+  freopen("4779.in", "r", stdin);
+  #endif
+
+  Init();
+  scanf("%d%d%d", &n, &m, &s);
+  while (m--) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    Add(a, b, c);
+  }
+  dijkstra();
+  for (int i = 1; i <= n; ++i) {
+    printf("%lld ", dis[i] == INF ? (1LL << 31) - 1 : dis[i]);
+  }
+  puts("");
+
+  return 0;
+}
