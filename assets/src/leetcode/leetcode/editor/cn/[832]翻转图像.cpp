13c155d493d7127eb557b74152288427236a9f3c
liuxueyang <liuxueyang457@gmail.com>
2021-03-07 17:02:26 +0800

    optimize leetcode 832 solution

--- assets/src/leetcode/leetcode/editor/cn/[832]翻转图像.cpp
+++ assets/src/leetcode/leetcode/editor/cn/[832]翻转图像.cpp
@@ -42,13 +42,15 @@ public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& a) {
         int n = a.size(), m = a[0].size();
         for (int i = 0; i < n; ++i) {
-            for (int j = 0, k = m - 1; j < k; ++j, --k) {
+            for (int j = 0, k = m - 1; j <= k; ++j, --k) {
                 swap(a[i][j], a[i][k]);
+                if (j != k) {
+                    a[i][j] = !a[i][j];
+                    a[i][k] = !a[i][k];
+                } else {
+                    a[i][j] = !a[i][j];
+                }
             }
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j)
-                a[i][j] = !a[i][j];
         }
         return a;
     }
