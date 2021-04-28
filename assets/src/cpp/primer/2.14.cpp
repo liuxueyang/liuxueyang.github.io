6e33f72c4e9601d6f57dc016706f0b76fd993f48
liuxueyang <liuxueyang457@gmail.com>
2021-04-28 20:45:54 +0800

    finish 2.13, 2.14

---
+++ assets/src/cpp/primer/2.14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int i = 100, sum = 0;
+  for (int i = 0; i != 10; ++i) {
+    sum += i;
+  }
+
+  cout << i << ' ' << sum << endl;
+
+  return 0;
+}
