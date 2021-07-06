b47f8d2376794e0c0d1d3b843044d85289b3a90e
liuxueyang <liuxueyang457@gmail.com>
2021-07-06 16:30:11 +0800

    finish  leetcode 1922

--- assets/src/leetcode/1922.cpp
+++ assets/src/leetcode/1922.cpp
@@ -1,8 +1,4 @@
-# -*- mode: snippet -*-
-# name: header
-# key: header
-# --
-// Date: `(current-time-string)`
+// Date: Tue Jul  6 16:26:51 2021
 
 #include <cstdio>
 #include <cstring>
@@ -24,6 +20,21 @@ const int dir[8][2] = {
   {1, 1}, {1, -1},
   {-1, 1}, {-1, -1},
 };
-
 typedef long long ll;
-typedef vector<int> VI;
+
+ll qmi(ll a, ll b, ll c) {
+  ll res = 1 % c;
+  while (b) {
+    if (b & 1) res = res * a % c;
+    a = a * a % c;
+    b >>= 1;
+  }
+  return res;
+}
+
+class Solution {
+public:
+    int countGoodNumbers(long long n) {
+      return qmi(5, (n + 1) / 2, MOD) * qmi(4, n / 2, MOD) % MOD;
+    }
+};
