05d59d5652712401ca9365c485ca4f63cb0da6f8
liuxueyang <liuxueyang457@gmail.com>
2021-10-30 11:02:17 +0800

    finish tree problems

---
+++ assets/src/leetcode/1038.cpp
@@ -0,0 +1,99 @@
+// Date: Sat Oct 30 10:57:20 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+typedef ListNode LN;
+typedef LN* LNP;
+typedef TreeNode TN;
+typedef TN* TNP;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int res;
+
+  void dfs(TNP ro) {
+    if (!ro) return;
+
+    auto l = ro->left, r = ro->right;
+    auto x = ro->val;
+
+    if (!l && !r) {
+      res += x;
+      ro->val = res;
+      return;
+    }
+
+    dfs(r);
+    res += x;
+    ro->val = res;
+    dfs(l);
+  }
+
+  TreeNode* bstToGst(TreeNode* ro) {
+    if (!ro) return ro;
+
+    res = 0;
+    dfs(ro);
+
+    return ro;
+  }
+};
---
+++ assets/src/leetcode/108.cpp
@@ -0,0 +1,93 @@
+// Date: Sat Oct 30 10:42:03 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+typedef ListNode LN;
+typedef LN* LNP;
+typedef TreeNode TN;
+typedef TN* TNP;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int n;
+  VI a;
+
+  TNP dfs(int i, int j) {
+    if (i > j) return nullptr;
+    else if (i == j) { return new TN(a[i]); }
+
+    int mid = (i + j) / 2;
+    TNP ro = new TN(a[mid]);
+
+    ro->left = dfs(i, mid - 1);
+    ro->right = dfs(mid + 1, j);
+
+    return ro;
+  }
+
+  TreeNode* sortedArrayToBST(vector<int>& _a) {
+    a = _a;
+    n = a.size();
+
+    return dfs(0, n - 1);
+  }
+};
