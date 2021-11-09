4f5cf81e6608dfd4aa14ef0efc2cfc135471a650
liuxueyang <liuxueyang457@gmail.com>
2021-11-09 23:01:31 +0800

    practice 7 DP easy problems on leetcode

---
+++ assets/src/leetcode/740_2.cpp
@@ -0,0 +1,79 @@
+// Date: Tue Nov  9 22:42:54 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int N = 10010, M = 20010;
+int c[N], d[M];
+
+class Solution {
+public:
+  int deleteAndEarn(vector<int>& a) {
+    int n = a.size();
+    memset(c, 0, sizeof c);
+    memset(d, 0, sizeof d);
+
+    for (auto &x : a) c[x]++;
+
+    d[1] = c[1];
+    for (int i = 2; i < N; ++i) {
+      d[i] = max(d[i - 2] + c[i] * i, d[i - 1]);
+    }
+
+    return d[N - 1];
+  }
+};
---
+++ assets/src/leetcode/746_1.cpp
@@ -0,0 +1,75 @@
+// Date: Tue Nov  9 22:26:25 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int N = 1010;
+int d[N];
+
+class Solution {
+public:
+  int minCostClimbingStairs(vector<int>& a) {
+    int n = a.size();
+    memset(d, 0x3f, sizeof d);
+    d[0] = a[0];
+    d[1] = a[1];
+    for (int i = 2; i < n; ++i) {
+      d[i] = min(d[i - 1], d[i - 2]) + a[i];
+    }
+    return min(d[n - 1], d[n - 2]);
+  }
+};
