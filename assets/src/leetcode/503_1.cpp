18d1b15dbf44765cfc2b7aac9f1479ad3f61730c
liuxueyang <liuxueyang457@gmail.com>
2021-11-10 20:07:09 +0800

    finish 4 stack & queue problems

---
+++ assets/src/leetcode/503_1.cpp
@@ -0,0 +1,80 @@
+// Date: Wed Nov 10 12:58:04 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int N = 20010;
+int stk[N];
+
+class Solution {
+public:
+  vector<int> nextGreaterElements(vector<int>& a) {
+    int n = a.size();
+    VI res(n, -1);
+    VI b(2 * n, 0);
+    for (int i = 0; i < n; ++i) b[i] = b[i + n] = a[i];
+
+    int tt = 0;
+    for (int i = 2 * n - 1; i >= 0; --i) {
+      while (tt && b[i] >= b[stk[tt]]) --tt;
+      if (i < n && tt) res[i] = b[stk[tt]];
+      stk[++tt] = i;
+    }
+
+    return res;
+  }
+};
