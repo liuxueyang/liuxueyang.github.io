ffe686a48ea7fa8df936b12b7f175365212a1f6e
liuxueyang <liuxueyang457@gmail.com>
2021-11-11 18:45:48 +0800

    finish 7 leetcode problems

---
+++ assets/src/leetcode/872_1.cpp
@@ -0,0 +1,97 @@
+// Date: Thu Nov 11 17:56:26 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  VI v1, v2;
+
+  void dfs(TNP ro, VI &v) {
+    if (!ro) return;
+
+    auto l = ro->left, r = ro->right;
+    if (!l && !r) {
+      v.push_back(ro->val);
+      return;
+    }
+
+    dfs(l, v);
+    dfs(r, v);
+  }
+
+  bool leafSimilar(TreeNode* ro1, TreeNode* ro2) {
+    v1 = VI{};
+    v2 = VI{};
+
+    dfs(ro1, v1);
+    dfs(ro2, v2);
+
+    return v1 == v2;
+  }
+};
