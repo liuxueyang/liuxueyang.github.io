902cb6c191e40df16e633c43ea087b838a898def
liuxueyang <liuxueyang457@gmail.com>
2021-11-13 12:20:19 +0800

    finish 3 leetcode problems

---
+++ assets/src/leetcode/102_1.cpp
@@ -0,0 +1,111 @@
+// Date: Sat Nov 13 10:27:18 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode* ro) {
+    vector<VI> res;
+
+    if (!ro) return res;
+
+    VI tmp;
+    queue<pair<TNP, int>> q;
+    int cur = 1;
+
+    q.push({ro, 1});
+
+    while (!q.empty()) {
+      auto t = q.front();
+      q.pop();
+
+      auto ver = t.first;
+      auto level = t.second;
+
+      if (level != cur) {
+        res.push_back(tmp);
+
+        tmp = VI{ver->val};
+        cur = level;
+      } else {
+        tmp.push_back(ver->val);
+      }
+
+      auto l = ver->left, r = ver->right;
+      if (l) q.push({l, level + 1});
+      if (r) q.push({r, level + 1});
+    }
+
+    if (!tmp.empty()) {
+      res.push_back(tmp);
+    }
+
+    return res;
+  }
+};
