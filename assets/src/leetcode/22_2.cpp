4be49e4159113b86b04ef88cda64846e2b1e51d7
liuxueyang <liuxueyang457@gmail.com>
2021-11-19 01:29:51 +0800

    finish leetcode 22

---
+++ assets/src/leetcode/22_2.cpp
@@ -0,0 +1,91 @@
+// Date: Fri Nov 19 01:23:46 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<string> res;
+  int n;
+
+  void dfs(int l, int r, string cur) {
+    if (l == r && l == n) {
+      res.push_back(cur);
+      return;
+    }
+
+    if (r > l ||
+        l + r >= 2 * n ||
+        l > n || r > n) return;
+
+    if (l == r) {
+      dfs(l + 1, r, cur + '(');
+    } else if (l > r) {
+      dfs(l + 1, r, cur + '(');
+      dfs(l, r + 1, cur + ')');
+    }
+  }
+
+  vector<string> generateParenthesis(int n_) {
+    res.clear();
+    n = n_;
+
+    dfs(0, 0, "");
+
+    return res;
+  }
+};
