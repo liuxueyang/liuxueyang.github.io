3b828806b11188ded9afdfe0148cb7ea87c211cc
liuxueyang <liuxueyang457@gmail.com>
2021-11-19 17:00:59 +0800

    practice 4 leetcode problems

---
+++ assets/src/leetcode/322_1.cpp
@@ -0,0 +1,78 @@
+// Date: Fri Nov 19 16:32:27 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int M = 10010;
+int d[M];
+
+class Solution {
+public:
+  int coinChange(vector<int>& a, int m) {
+    int n = a.size();
+    memset(d, 0x3f, sizeof d);
+    d[0] = 0;
+
+    for (int i = 0; i < n; ++i) {
+      for (int v = a[i]; v <= m; ++v) {
+        d[v] = min(d[v], d[v - a[i]] + 1);
+      }
+    }
+
+    return d[m] == INF ? -1 : d[m];
+  }
+};
