b8af3907a4e2e3236331aef2a136ec45b8dbc0a3
liuxueyang <liuxueyang457@gmail.com>
2022-01-08 20:32:53 +0800

    finish 2 problems

---
+++ assets/src/AcWing/4206.cpp
@@ -0,0 +1,85 @@
+// Date: Sat Jan  8 20:16:39 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+int main(void)
+{
+#ifdef _DEBUG
+  freopen("4206.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+  string n;
+  while (cin >> n) {
+    int c4 {}, c7 {};
+
+    for (auto &c : n) {
+      if (c == '4') c4++;
+      else if (c == '7') c7++;
+    }
+
+    int c = c4 + c7;
+    if (c == 4 || c == 7) cout << "YES\n";
+    else cout << "NO\n";
+  }
+
+  return 0;
+}
