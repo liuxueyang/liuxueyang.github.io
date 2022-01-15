95cd5cc0b8236654cc5528000136c1b0856c1f0b
liuxueyang <liuxueyang457@gmail.com>
2022-01-15 08:39:56 +0800

    add an optimized solution for leetcode 1716

---
+++ 1716_1.cpp
@@ -0,0 +1,77 @@
+// Date: Sat Jan 15 08:36:25 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  int totalMoney(int n) {
+    int s0 = 7 + 7 * (7 - 1) / 2,
+      n1 = n / 7, n2 = n % 7,
+      a0 = n1 + 1;
+
+    int res = n1 * s0 + n1 * (n1 - 1) / 2 * 7;
+    res += n2 * a0 + n2 * (n2 - 1) / 2;
+
+    return res;
+  }
+};
