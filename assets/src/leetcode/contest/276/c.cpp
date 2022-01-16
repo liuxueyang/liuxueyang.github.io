370908b08de11778976cb92f3c12e87043e2fef3
liuxueyang <liuxueyang457@gmail.com>
2022-01-16 18:57:39 +0800

    add leetcode contest 276

---
+++ assets/src/leetcode/contest/276/c.cpp
@@ -0,0 +1,86 @@
+// Date: Sun Jan 16 10:51:57 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  long long mostPoints(vector<vector<int>>& q) {
+    int n = q.size();
+    vector<ll> d(n, 0);
+    ll res = 0;
+
+    d[n - 1] = q[n - 1][0];
+    res = d[n - 1];
+
+    for (int i = n - 2; i >= 0; --i) {
+      int i1 = i + q[i][1] + 1;
+      ll rest = 0;
+      if (i1 <= n - 1) rest = d[i1];
+
+      d[i] = max(q[i][0] + rest, d[i + 1]);
+      res = max(res, d[i]);
+    }
+
+    return res;
+  }
+};
