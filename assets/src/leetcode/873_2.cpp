ab3ca920188c266502bb09ac6cafc8da798aabe7
liuxueyang <liuxueyang457@gmail.com>
2022-07-09 17:52:51 +0800

    2 leetcode problems

---
+++ assets/src/leetcode/873_2.cpp
@@ -0,0 +1,100 @@
+// Date: Sat Jul  9 13:16:16 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int N = 1010;
+int d[N][N];
+
+class Solution {
+public:
+  int lenLongestFibSubseq(vector<int>& a) {
+    int n = SZ(a);
+    unordered_map<int, int> pos;
+    REP(i, 0, n) pos[a[i]] = i;
+
+    REP(i, 0, n) REP(j, 0, i) d[i][j] = 2;
+
+    int ans = 0;
+    REP(i, 2, n) {
+      REP(j, 1, i) {
+        int x = a[i] - a[j];
+        if (has(pos, x) && x < a[j]) {
+          int k = pos[x];
+          d[i][j] = max(d[i][j], d[j][k] + 1);
+          ans = max(ans, d[i][j]);
+        }
+      }
+    }
+
+    if (ans <= 2) ans = 0;
+    return ans;
+  }
+};
