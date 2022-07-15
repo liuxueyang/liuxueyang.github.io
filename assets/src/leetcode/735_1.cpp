02b888845773f24def055f961c97be319c2feff9
liuxueyang <liuxueyang457@gmail.com>
2022-07-15 09:47:38 +0800

    leetcode 735

---
+++ assets/src/leetcode/735_1.cpp
@@ -0,0 +1,111 @@
+// Date: Fri Jul 15 09:17:54 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<int> asteroidCollision(vector<int>& a) {
+    stack<int> s1, s2;
+
+    for (auto x : a) {
+      if (x < 0) {
+        if (s1.empty()) {
+          s2.push(x);
+          continue;
+        }
+
+        while (nonempty(s1) && s1.top() <= abs(x)) {
+          if (s1.top() == abs(x)) {
+            s1.pop();
+            x = 0;
+            break;
+          }
+
+          s1.pop();
+        }
+
+        if (s1.empty() && x) s2.push(x);
+      } else {
+        s1.push(x);
+      }
+    }
+
+    VI res;
+    while (nonempty(s1)) {
+      res.pb(s1.top()); s1.pop();
+    }
+    while (nonempty(s2)) {
+      res.pb(s2.top()); s2.pop();
+    }
+    reverse(all(res));
+    return res;
+  }
+};
