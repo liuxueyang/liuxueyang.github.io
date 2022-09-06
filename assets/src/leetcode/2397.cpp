268886d91ab9152c2f6c3092911c7ad3f259c91f
liuxueyang <liuxueyang457@gmail.com>
2022-09-06 23:53:24 +0800

    finish 2397

---
+++ assets/src/leetcode/2397.cpp
@@ -0,0 +1,113 @@
+// Date: Tue Sep  6 23:38:34 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<VI> a;
+  int n, m, res;
+
+  int cnt1(int n) {
+    int res {};
+    while (n) {
+      n -= (n & (-n));
+      res++;
+    }
+    return res;
+  }
+
+  int maximumRows(vector<vector<int>>& mat, int cols) {
+    a = mat;
+    n = SZ(a); m = SZ(a[0]);
+    res = 0;
+    VI b(n, 0);
+
+    REP(i, 0, n) {
+      int cur = 0;
+      PER(j, 0, m) {
+        cur = cur * 2 + a[i][j];
+      }
+      b[i] = cur;
+    }
+
+    REP(i, 0, (1 << 13)) {
+      int cur = 0;
+      if (cnt1(i) != cols) continue;
+
+      REP(j, 0, n) {
+        if (b[j] == (b[j] & i)) cur++;
+      }
+      res = max(res, cur);
+    }
+
+    return res;
+  }
+};
