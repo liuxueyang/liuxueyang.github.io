1ad5c4daaff57e9cc1e483e1cbdef4b112785ee2
liuxueyang <liuxueyang457@gmail.com>
2022-09-09 09:54:39 +0800

    leetcode 2381

---
+++ assets/src/leetcode/problemset/2381.cpp
@@ -0,0 +1,104 @@
+// Date: Fri Sep  9 09:11:41 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int N = 50010;
+int b[N];
+
+class Solution {
+public:
+  string shiftingLetters(string s, vector<vector<int>>& a) {
+    int n = SZ(s), m = SZ(a);
+    memset(b, 0, sizeof b);
+    REP(i, 0, m) {
+      int x = a[i][0], y = a[i][1], z = a[i][2];
+      if (z) {
+        b[x]++;
+        b[y + 1]--;
+      } else {
+        b[x]--;
+        b[y + 1]++;
+      }
+    }
+
+    b[0] = ((b[0] % 26) + 26) % 26;
+    REP(i, 1, n) {
+      b[i] = ((b[i - 1] + b[i]) % 26 + 26) % 26;
+    }
+
+    REP(i, 0, n) {
+      int x = s[i] - 'a';
+      x = (x + b[i]) % 26;
+      s[i] = 'a' + x;
+    }
+    return s;
+  }
+};
