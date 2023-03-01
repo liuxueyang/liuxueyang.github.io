86e10cb6137d930c2ac81f2ae20318eb1354f2b5
liuxueyang <liuxueyang457@gmail.com>
2023-03-01 22:41:41 +0800

    Finish 3 leetcode problems

---
+++ assets/src/leetcode/problemset/2574.cpp
@@ -0,0 +1,101 @@
+// Date: Wed Mar  1 20:59:06 2023
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<int> leftRigthDifference(vector<int>& a) {
+    int n = SZ(a);
+    VI pre(n, 0), post(n, 0);
+
+    pre[0] = a[0];
+    REP(i, 1, n) {
+      pre[i] = pre[i - 1] + a[i];
+    }
+
+    post[n - 1] = a[n - 1];
+    PER(i, 0, n - 1) {
+      post[i] = post[i + 1] + a[i];
+    }
+
+    VI res(n, 0);
+
+    REP(i, 0, n) {
+      int left = 0, right = 0;
+      if (i) left = pre[i - 1];
+      if (i < n - 1) right = post[i + 1];
+
+      res[i] = abs(left - right);
+    }
+
+    return res;
+  }
+};
